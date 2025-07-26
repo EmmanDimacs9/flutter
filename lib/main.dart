eee5e4f979bee807e943d34d87059ca04e8994df
EmmanDimacs9 <isekaisenpai9@gmail.com>
2025-07-26 14:45:31 +0800

    fix error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-flutteimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // ignore: depend_on_referenced_packages
-import '../lib/main.dart';
+import 'package:ict_services_management/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
